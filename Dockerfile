429822008114d51a0621a4de8f8bd08b5961eea6
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2022-05-01 02:00:40 +0100

    feat:  http server add

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM node:17-slim
 
-RUN apt-get update && apt-get install -y \
+RUN apt-get update && apt-get install -y --no-install-recommends\
     git-all
 WORKDIR /app
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,39 @@
+import { createServer } from "node:http";
+import { randomUUID } from "node:crypto";
+import { on } from "node:events";
 
-async function bootstrap(){
-  console.log(`workdir: just testing`);
+const users = new Set();
+
+const app = createServer((req, res) => {
+  if (req.method == "GET" && req.url == "/") {
+    res.writeHead(200, { "Content-Type": "text/json" });
+    res.end(JSON.parse(users));
+  }
+  if (req.method == "POST" && req.url == "/") {
+    on("data", (data) => {
+      const user = {
+        id: randomUUID(),
+        name: data.name,
+        email: data.email,
+      };
+      users.add(user);
+      console.log(user);
+    });
+    const user = JSON.parse(body);
+    users.add(user);
+    res.end(
+      JSON.stringify({
+        id: user.id,
+        name: user.name,
+      })
+    );
+  }
+});
+
+async function bootstrap() {
+  app.listen(process.env.PORT || 3333, function () {
+    console.log("server started");
+  });
 }
-bootstrap()
+bootstrap();
 console.log(workdir);
