424ce8a40139994818a1ccd2004a508ef45d8d15
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2022-05-01 02:34:48 +0100

    refactore: create user

--- src/index.js
+++ src/index.js
@@ -2,31 +2,27 @@ import { createServer } from "node:http";
 import { randomUUID } from "node:crypto";
 import { on } from "node:events";
 
-const users = new Set();
+const users = [];
 
 const app = createServer((req, res) => {
   if (req.method == "GET" && req.url == "/") {
     res.writeHead(200, { "Content-Type": "text/json" });
-    res.end(JSON.parse(users));
+    res.end(JSON.stringify(users));
   }
   if (req.method == "POST" && req.url == "/") {
-    on("data", (data) => {
+    req.on("data", async (d) => {
+      const data = await JSON.parse(d);
       const user = {
         id: randomUUID(),
         name: data.name,
         email: data.email,
       };
-      users.add(user);
+
+      users.push(user);
       console.log(user);
     });
-    const user = JSON.parse(body);
-    users.add(user);
-    res.end(
-      JSON.stringify({
-        id: user.id,
-        name: user.name,
-      })
-    );
+    res.writeHead(201, { "Content-Type": "text/json" });
+    res.end();
   }
 });
 
@@ -36,4 +32,3 @@ async function bootstrap() {
   });
 }
 bootstrap();
-console.log(workdir);
