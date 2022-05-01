7dc31715c2d727a9f1a8c88320050696bb55cd95
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2022-05-01 23:03:03 +0100

    feat:stream route add

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:17-slim
 
 RUN apt-get update && apt-get install -y --no-install-recommends\
-    git-all
+    git-all zsh fonts-powerline gpg gpg-agent gnupg socat
 WORKDIR /app
 
 COPY package.json  package-lock.json /app/
--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
+import {Readable,Writable} from 'node:stream'
 import { createServer } from "node:http";
 import { randomUUID } from "node:crypto";
-import { on } from "node:events";
-
+import {faker} from "@faker-js/faker"
 const users = [];
 
+function * generatorUser(maxUsers=100){
+   for(let i=0;i<maxUsers;i++){
+           const user={
+             id:randomUUID(),
+             name:faker.name.firstName('female'),
+             address:faker.address.city(),
+           }
+           yield user;
+   }
+}
+
 const app = createServer((req, res) => {
-  if (req.method == "GET" && req.url == "/") {
+  if (req.method === "GET" && req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/json" });
     res.end(JSON.stringify(users));
   }
-  if (req.method == "POST" && req.url == "/") {
+  if (req.method === "POST" && req.url === "/") {
     req.on("data", async (d) => {
       const data = await JSON.parse(d);
       const user = {
@@ -23,6 +34,21 @@ const app = createServer((req, res) => {
     });
     res.writeHead(201, { "Content-Type": "text/json" });
     res.end();
+  }
+  if(req.method==="GET"&&req.url==='/dados'){
+      new Readable(
+        {
+          read(){
+            for(const data of generatorUser()){
+              this.push(JSON.stringify(data));
+              console.log(data);
+            }
+            this.push(null)
+          }
+        }
+      ).pipe(
+        res
+      )    
   }
 });
 
